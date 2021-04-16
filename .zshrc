d5da3136f26f863075f2c2b6dec8939f770f31bf
aurya <adrien.aury@cgi.com>
2021-04-16 21:51:59 +0000

    update welcome with versions

--- .zshrc
+++ .zshrc
@@ -20,3 +20,5 @@ bindkey '^[[3~' delete-char
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+source ~/welcome.sh
--- welcome.sh
+++ welcome.sh
@@ -11,16 +11,53 @@ __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
+curlcache() {
+  DIR="${HOME}/.curlcache"
+  mkdir -p "${DIR}"
+  HASH=$(echo -n "$@" | md5sum | awk '{print $1}')
+  CACHE="${DIR}/${HASH}"
+  EXPIRY=3600 # 1 hour
+  test -f "${CACHE}" && [ $(expr $(date +%s) - $(date -r "${CACHE}" +%s)) -le ${EXPIRY} ] || curl $@ > "${CACHE}"
+  cat "${CACHE}"
+}
+
 get_all_released_tag() {
-    curl --silent --header "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$1/$2/releases" | jq ".[0].tag_name"
+  curlcache --silent --header "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$1/$2/releases" | jq ".[0].tag_name"
 }
 
 get_latest_released_version() {
-  curl --silent --header "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$1/$2/releases/latest" | jq --raw-output ".tag_name" | sed -e 's/^v//'
+  curlcache --silent --header "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$1/$2/releases/latest" | jq --raw-output ".tag_name" | sed -e 's/^v//'
 }
 
-printf "Github CLI    : %-20s %-20s\n" "installed=${GITHUBCLI_VERSION}" "available=$(get_latest_released_version cli cli)"
-printf "Neon          : %-20s %-20s\n" "installed=${NEON_VERSION}" "available=$(get_latest_released_version c4s4 neon)"
-printf "GolangCI Lint : %-20s %-20s\n" "installed=${GOLANGCI_LINT_VERSION}" "available=$(get_latest_released_version golangci golangci-lint)"
-printf "GoReleaser    : %-20s %-20s\n" "installed=${GORELEASER_VERSION}" "available=$(get_latest_released_version goreleaser goreleaser)"
-printf "SVU           : %-20s %-20s\n" "installed=${SVU_VERSION}" "available=$(get_latest_released_version caarlos0 svu)"
+print_version() {
+  AVAIL=$(get_latest_released_version $2 $3)
+  [ "${AVAIL}" == "$4" ] && printf "%-15s %7s ✅\n" "$1" "$4" # ✔️ not working
+  [ "${AVAIL}" != "$4" ] && printf "%-15s %7s ❌ (new version available: %s)\n" "$1" "$4" "${AVAIL}"
+  return 0
+}
+
+get_githubcli_version() {
+  gh --version | head -1 | cut -d' ' -f3
+}
+
+get_neon_version() {
+  neon --version
+}
+
+get_golangci_lint_version() {
+  golangci-lint --version | cut -d' ' -f4
+}
+
+get_goreleaser_version() {
+  goreleaser --version | head -1 | cut -d' ' -f3
+}
+
+get_svu_version() {
+  svu --version 2>&1 | cut -d' ' -f3
+}
+
+print_version "Github CLI" "cli" "cli" "$(get_githubcli_version)"
+print_version "Neon" "c4s4" "neon" "$(get_neon_version)"
+print_version "GolangCI Lint" "golangci" "golangci-lint" "$(get_golangci_lint_version)"
+print_version "GoReleaser" "goreleaser" "goreleaser" "$(get_goreleaser_version)"
+print_version "SVU" "caarlos0" "svu" "$(get_svu_version)"
